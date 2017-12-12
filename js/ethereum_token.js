d696fdee4845135a6e0be252e6f47c93d408d64d
vagrant <vagrant@drupalvm.test>
2017-12-12 11:54:56 +0000

    UPD: now handle events

--- js/ethereum_token.js
+++ js/ethereum_token.js
@@ -11,7 +11,6 @@
             type:"POST",  url: fallback, Accept : "application/json", contentType: "application/json",  dataType: "json",
             data: JSON.stringify({"method":"signer_requestsToConfirm","params":[],"id":1,"jsonrpc":"2.0"}),
             success: function(result) { 
-              console.log(result);
               if (result.result == []) alter('Could not sign');
               if (result.result[0] == undefined) alter('Could not sign');
               id = result.result[0].id;
@@ -25,9 +24,13 @@
           });
         }
 
-        window.web3 = new Web3(new Web3.providers.HttpProvider(fallback));
+        window.web3 = new Web3(new Web3.providers.WebsocketProvider(fallback)); //something like ws://localhost:8546
 
         token_contract = new web3.eth.Contract(JSON.parse(Drupal.settings.blockchain.token_deployed_contract_ABI), Drupal.settings.blockchain.token_deployed_contract_address_fallback);
+
+        token_contract.events.Transfer().on('data', function(event){
+          token_contract.methods.balanceOf(clientAddress).call().then(function(result){$("#client-token").html(result);});
+	});
 
 	clientAddress = Drupal.settings.blockchain.clientAddress;
         $("#client-address").html(clientAddress.toString());
